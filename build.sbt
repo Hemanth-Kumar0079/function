96bdb486bc666502e8de82dc54ea172e18fb11f2
Hemanth-Kumar0079 <yhksai@gmail.com>
2020-03-09 20:02:49 -0400

    FunctionPractice

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "function"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/FunctionPractice.scala
@@ -0,0 +1,5 @@
+object FunctionPractice extends App {
+  def addOne(in: Int): Int = in + 1
+
+  println(addOne(10))
+}
